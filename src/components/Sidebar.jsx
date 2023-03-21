be67a781d1c29e382202bdbe4dc430970a225a97
CharlyLuna <cluna10@ucol.mx>
2023-03-20 21:37:58 -0600

    Make sidebar responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,8 +30,9 @@ export const Sidebar = () => {
   }, [session, spotifyApi])
 
   return (
-    <div className='text-gray-500 p-5 text-sm border-r
-    border-gray-900 overflow-y-scroll h-screen scrollbar-hide'
+    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r
+    border-gray-900 overflow-y-scroll h-screen scrollbar-hide
+    sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex'
     >
       <div className='space-y-4'>
 
@@ -83,7 +84,7 @@ export const Sidebar = () => {
         </button>
         <hr className='border-t-[0.1px] border-gray-900' />
 
-        {/* PLaylist */}
+        {/* PLaylists */}
         {
           playlists.map((playlist) => (
             <p key={playlist.id} onClick={() => setPlaylistId(playlist.id)} className='cursor-pointer hover:text-white'>{playlist.name}</p>
